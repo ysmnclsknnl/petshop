81f041f0e7be1d21a721ff596c8d2d175f1bbcc7
Yasemin Caliskan <>
2023-07-17 18:17:56 +0200

    refactor: implement SuperPet super class

--- src/main/java/com/example/petshop/PetWithStringImage.java
+++ src/main/java/com/example/petshop/PetWithStringImage.java
@@ -1,37 +1,33 @@
 package com.example.petshop;
 
 import com.example.petshop.collection.Pet;
-import com.example.petshop.serializer.ObjectIdSerializer;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-
-
 import java.util.Base64;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PetWithStringImage {
-    @Id
-    @JsonSerialize(using = ObjectIdSerializer.class)
-    private ObjectId id;
-    private String name;
-    private String description;
-    private Integer age;
-    private Type type;
-    private Boolean adopted = false;
+public class PetWithStringImage extends SuperPet implements Comparable<PetWithStringImage> {
     private String photo;
 
     public PetWithStringImage(Pet pet) {
-            this.id = pet.getId();
-            this.name = pet.getName();
-            this.description = pet.getDescription();
-            this.age = pet.getAge();
-            this.type = pet.getType();
-            this.photo = Base64.getEncoder().encodeToString(pet.getPhoto().getData());
+        super(pet.getId(), pet.getName(),pet.getDescription(),pet.getAge(),pet.getType(),pet.getAdopted());
+        this.photo = Base64.getEncoder().encodeToString(pet.getPhoto().getData());
     }
+
+    @Override
+    public int compareTo(PetWithStringImage other) {
+        long thisTimestamp = this.getId().getTimestamp();
+        long otherTimestamp = other.getId().getTimestamp();
+
+        if (thisTimestamp < otherTimestamp) {
+            return 1;
+        } else if (thisTimestamp > otherTimestamp) {
+            return -1;
+        }
+        return 0;
+    }
+
 }
--- src/main/java/com/example/petshop/collection/Pet.java
+++ src/main/java/com/example/petshop/collection/Pet.java
@@ -1,50 +1,35 @@
 package com.example.petshop.collection;
 
-import com.example.petshop.PetWithStringImage;
-import com.example.petshop.Type;
+import com.example.petshop.SuperPet;
 import com.example.petshop.serializer.BinaryDeserializer;
-import com.example.petshop.serializer.ObjectIdSerializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.Binary;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.Base64;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "pet")
-public class Pet {
-    @Id
-    @JsonSerialize(using = ObjectIdSerializer.class)
-    private ObjectId id;
-    private String name;
-    private String description;
-    private Integer age;
-    private Type type;
-    private Boolean adopted = false;
+public class Pet extends SuperPet{
     @JsonDeserialize(using = BinaryDeserializer.class)
     private Binary photo;
 
-    public Pet(String name, String description, Integer age, Type type, Binary photo) {
-        this.name = name;
-        this.description = description;
-        this.age = age;
-        this.type = type;
-        this.photo = photo;
+    public Pet(SuperPet superPet, MultipartFile photo) throws IOException {
+        super(superPet.getName(), superPet.getDescription(), superPet.getAge(),superPet.getType(), false);
+        if (!photo.isEmpty()) {
+            byte[] photoBytes = photo.getBytes();
+            String base64String = Base64.getEncoder().encodeToString(photoBytes);
+            byte[] binaryData = Base64.getDecoder().decode(base64String);
+            this.setPhoto(new Binary(binaryData));
+        } else {
+            throw new IllegalArgumentException(" A pet must have a photo");
+        }
     }
-    public Pet(PetWithStringImage pet) {
-        byte[] binaryData = Base64.getDecoder().decode(pet.getPhoto());
-        this.name = pet.getName();
-        this.description = pet.getDescription();
-        this.age = pet.getAge();
-        this.type = pet.getType();
-        this.photo = new Binary(binaryData);
-    }
+
 }
