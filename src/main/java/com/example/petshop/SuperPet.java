0abdd0c94303fd444f40190ca0e134127a0aebf5
Yasemin Caliskan <>
2023-07-17 18:17:56 +0200

    refactor: implement SuperPet super class

---
+++ src/main/java/com/example/petshop/SuperPet.java
@@ -0,0 +1,31 @@
+package com.example.petshop;
+
+import com.example.petshop.serializer.ObjectIdSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuperPet {
+    @Id
+    @JsonSerialize(using = ObjectIdSerializer.class)
+    private ObjectId id;
+    private String name;
+    private String description;
+    private Integer age;
+    private Type type;
+    private Boolean adopted = false;
+
+    public SuperPet(String name, String description, Integer age, Type type, Boolean adopted) {
+        this.name = name;
+        this.description = description;
+        this.age = age;
+        this.type = type;
+        this.adopted = adopted;
+    }
+}
