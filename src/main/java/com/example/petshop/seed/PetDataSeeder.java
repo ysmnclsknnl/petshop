727c2ca7648f19e6f4a84ecbbed604779baa6bf2
Yasemin Caliskan <>
2023-07-17 18:00:30 +0200

    refactor: use faker to create fake data

--- src/main/java/com/example/petshop/seed/PetDataSeeder.java
+++ src/main/java/com/example/petshop/seed/PetDataSeeder.java
@@ -2,47 +2,41 @@ package com.example.petshop.seed;
 
 import com.example.petshop.Type;
 import com.example.petshop.collection.Pet;
+import com.example.petshop.repository.PetRepository;
+import net.datafaker.Faker;
 import org.bson.types.Binary;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
-
-import java.util.Random;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Component
 public class PetDataSeeder {
-    private final MongoTemplate mongoTemplate;
+    private final PetRepository petRepository;
 
     @Autowired
-    public PetDataSeeder(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
+    public PetDataSeeder(PetRepository petRepository) {
+        this.petRepository = petRepository;
     }
 
     public void seedData() {
-        IntStream.range(0, 10)
-                .mapToObj(i -> {
-                    Pet pet = new Pet();
-                    pet.setId(new ObjectId());
-                    pet.setName("Pet " + i);
-                    pet.setDescription("Description for Pet " + i);
-                    pet.setAge(i);
-                    pet.setType(Type.DOG);
-                    pet.setAdopted(i % 2 == 0);
-                    pet.setPhoto(new Binary(generateFakePhoto()));
-                    return pet;
-                })
-                .forEach(mongoTemplate::save);
+        Faker faker = new Faker();
+        List<Pet> pets = IntStream.range(0, 5)
+                .mapToObj(i -> createPet(faker))
+                .collect(Collectors.toList());
+
+        petRepository.saveAll(pets);
     }
-
-    private byte[] generateFakePhoto() {
-        int maxSize = 100 * 1024;
-        byte[] photoData = new byte[maxSize]; // Adjust the size as per your requirements
-
-        new Random().nextBytes(photoData);
-
-        return photoData;
+    private Pet createPet(Faker faker) {
+        Pet pet = new Pet();
+        pet.setName(faker.cat().name());
+        pet.setDescription("Lovely pet. Enjoys playing with its owner.");
+        pet.setAge(faker.number().numberBetween(0, 10));
+        pet.setType(Type.CAT);
+        pet.setAdopted(faker.bool().bool());
+        pet.setPhoto(new Binary(faker.photography().camera().getBytes()));
+        return pet;
     }
 }
 
