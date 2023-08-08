a20a65074cf7bb964139d00ae220a3e4e115ca29
Yasemin Caliskan <>
2023-08-08 12:05:07 +0200

    add test case for register

---
+++ src/test/java/com/example/petshop/user/controller/AuthControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.petshop.user.controller;
+
+import com.example.petshop.pet.data.Pet;
+import com.example.petshop.pet.data.PetRepository;
+import com.example.petshop.pet.data.Type;
+import com.example.petshop.user.data.User;
+import com.example.petshop.user.data.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+class AuthControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setup() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void testRegisterUser() throws Exception {
+        String requestBody = "{\"userName\": \"aaa@gmail.com\", \"password\": \"1234567Ab$\", \"role\": \"CUSTOMER\"}";
+
+        final String content = mvc.perform(post("/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertEquals(content, "aaa@gmail.com");
+
+        User savedUser = userRepository.findByEmail("aaa@gmail.com").get();
+        assertEquals(savedUser.getUsername(), "aaa@gmail.com");
+    }
+
+}
