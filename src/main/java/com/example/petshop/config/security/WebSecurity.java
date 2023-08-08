f7485b601eeef231ccfbd10a6de16d37d13a5156
Yasemin Caliskan <>
2023-08-08 09:19:18 +0200

    turnoff security

--- src/main/java/com/example/petshop/config/security/WebSecurity.java
+++ src/main/java/com/example/petshop/config/security/WebSecurity.java
@@ -9,12 +9,14 @@ import com.nimbusds.jose.proc.SecurityContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -30,6 +32,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
+@ConditionalOnMissingBean(SecurityFilterChain.class)
 public class WebSecurity {
     final
     JwtToUserConverter jwtToUserConverter;
@@ -54,21 +57,21 @@ public class WebSecurity {
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
-                .authorizeHttpRequests((authorize) -> authorize
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/pets/add").hasRole("ADMIN")
                         .requestMatchers("/pets/{id}").hasRole("CUSTOMER")
                         .requestMatchers("/pets").hasAnyRole("CUSTOMER","ADMIN")
                         .anyRequest().authenticated())
-                .logout(logout -> logout.permitAll())
+                .logout(LogoutConfigurer::permitAll)
 
                 .formLogin(login ->
                         login.loginPage("/login"))
-                .oauth2ResourceServer((oauth2) ->
-                        oauth2.jwt((jwt) -> jwt.jwtAuthenticationConverter(jwtToUserConverter))
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtToUserConverter))
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .exceptionHandling((exceptions) -> exceptions
+                .exceptionHandling(exceptions -> exceptions
                         .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
                         .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
                 );
